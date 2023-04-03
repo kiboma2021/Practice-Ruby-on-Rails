026787b1b34f1e425db7a4a256d06592faf07b08
kiboma <omayioben@gmail.com>
2023-04-03 08:42:20 +0300

    Add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'likes/index'
+  #get 'likes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "users#index"
 
-   get "/comments", to: "comments#index"
-   get "/posts", to: "posts#index"
-   get "/likes", to: "likes#index"
+   resources :users
+
+ #  get "/users", to: "users#index"
+ #  get "/users/:id", to: "users#show"
+ #  get "/comments", to: "comments#index"
+ #  get "/posts", to: "posts#index"
+ #  get "/likes", to: "likes#index"
 end
